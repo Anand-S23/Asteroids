9e1c2a6122ba19667715797b804395ec625f4071
Anand-S23 <anandsroopsingh23@gmail.comgit config --global user.email anandsroopsingh23@gmail.comgit config --global user.email anandsroopsingh23@gmail.com>
2019-12-09 17:51:47 -0500

    initial commit

---
+++ game.c
@@ -0,0 +1,16 @@
+// Main Game FIle
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "game.h"
+
+// Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGH = 480;
+
+int main(int argc, char* args[])
+{
+    printf("Hello world\n");
+    return 0;
+}
